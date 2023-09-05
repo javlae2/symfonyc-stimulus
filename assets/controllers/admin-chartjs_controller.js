83d08aeac5cb8bce123a998f24374ecbf28c3c97
Manuel Escobar <mescobar@pragawebstudio.com>
2023-09-05 19:38:29 +0200

    added chartjs with fake data with chartjs for stimulus - chapter 25

---
+++ assets/controllers/admin-chartjs_controller.js
@@ -0,0 +1,20 @@
+import {Controller} from "@hotwired/stimulus";
+
+export default class extends Controller{
+    onChartConnect(event) {
+        this.chart = event.detail.chart;
+
+        this.changeDataSets();
+    }
+
+    changeDataSets(){
+        setTimeout(() => {this.setNewData(); this.changeDataSets()}, 2000)
+    }
+
+    setNewData() {
+        const newNumber = Math.floor(Math.random() * 50) + 1;
+        console.log(newNumber);
+        this.chart.data.datasets[0].data[Math.floor(Math.random() * (this.chart.data.datasets[0].data.length))] = newNumber;
+        this.chart.update();
+    }
+}
